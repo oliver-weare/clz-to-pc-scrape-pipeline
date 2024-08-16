8cf87ed1755db82f2a64d7461f85767cf6f3eb21
Oliver Weare <oliver_weare@protonmail.com>
2024-08-16 21:27:48 +0100

    use csv module for file data instead

--- lib/files.py
+++ lib/files.py
@@ -1,8 +1,12 @@
-import pandas
+import csv
 
 from paths import INPUT_FILE_PATH
 
 
 def get_csv_data():
-    csv_data = pandas.read_csv(INPUT_FILE_PATH)
-    return csv_data
+    with open(INPUT_FILE_PATH) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        data = [row for row in csv_reader]
+
+        return data
