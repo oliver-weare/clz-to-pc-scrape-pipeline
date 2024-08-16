8cb1bfb1b96a0e00176287a2340233fb01cc7638
Oliver Weare <oliver_weare@protonmail.com>
2024-08-17 00:57:46 +0100

    file write

--- lib/files.py
+++ lib/files.py
@@ -1,12 +1,23 @@
-import csv
-
-from paths import INPUT_FILE_PATH
+import csv, os
 
 
-def get_csv_data():
-    with open(INPUT_FILE_PATH) as csv_file:
+def delete_file_if_exists(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+
+def get_csv_data(file_path):
+    with open(file_path) as csv_file:
         csv_reader = csv.reader(csv_file)
         next(csv_reader)
         data = [row for row in csv_reader]
 
         return data
+
+
+def write_csv_file(file_path, data):
+    with open(file_path, mode="w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        for row in data:
+            csv_writer.writerow(row)
